b2698219b76eda006da42e89a32f2a438098b1f6
dimitar-grigorov <mitak.backtrack@gmail.com>
2023-11-12 08:03:05 +0200

    Added README. Added comments.

---
+++ README.md
@@ -0,0 +1,27 @@
+# CGMiner Monitor
+
+This is a Python script for monitoring CGMiner.
+
+## Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/dimitar-grigorov/cgminer-monitor.git
+```
+
+2. Navigate to the project directory:
+
+```bash
+cd cgminer-monitor
+```
+
+3. Install the required Python packages:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+Run the script with the following command:
--- cgminer-monitor.py
+++ cgminer-monitor.py
@@ -2,46 +2,54 @@
 import argparse
 import pycgminer
 
-
-
+# Entry point of the script
 if __name__ == '__main__':
+    # Help text for mode argument
     modeHelp = "How the output will be formatted. Use either Default or PRTG."
+    
+    # Initialize argument parser
     parser = argparse.ArgumentParser()
+    
+    # Define script arguments
     parser.add_argument('-a', '--host', required=True)
     parser.add_argument('-p', '--port', type=int, default=4028)
     parser.add_argument('-m', '--mode', default="Default", help=modeHelp)
     parser.add_argument('-r', '--min-rate', default=0, type=float, help="Min rate. Use only with --mode=PRTG")
+    
+    # Parse arguments
     args = parser.parse_args()
 
-# Store arguments in variable
+# Store parsed arguments in variables
 minerHost = args.host
 minerPort = args.port
-minRate = float(args.min_rate);
+minRate = float(args.min_rate)
 prtgMode = args.mode == "PRTG"
 
+# Check if min-rate is positive when in PRTG mode
 if prtgMode and (minRate == 0):
-    print "--min-rate is required to be positive in PRTG mode"
+    print("--min-rate is required to be positive in PRTG mode")
     exit(1)
 
-# Setup API object
+# Initialize CgminerAPI object
 cgminer = pycgminer.CgminerAPI()
 cgminer.host = minerHost
 cgminer.port = int(minerPort)
 
-# All output of the command "summary"
+# Get summary from cgminer
 summary = cgminer.command('summary')
-hashesPerFive = summary[unicode('SUMMARY')][0].get(unicode('GHS 5s'), None)
+hashesPerFive = summary[str('SUMMARY')][0].get(str('GHS 5s'), None)
 hashesPerFive = float(hashesPerFive)
-# Is hashes None?
+
+# Check if hashesPerFive is None
 if not hashesPerFive:
     exit(1)
 
-# PRTG mode
+# Output formatting for PRTG mode
 if prtgMode:
     if hashesPerFive < minRate:
-        print '{0}:ERROR'.format(hashesPerFive)
+        print('{0}:ERROR'.format(hashesPerFive))
     else:
-        print '{0}:OK'.format(hashesPerFive)
-# Default mode
+        print('{0}:OK'.format(hashesPerFive))
+# Output formatting for Default mode
 else:
-    print hashesPerFive
+    print(hashesPerFive)
